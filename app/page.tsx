60fc25495e220ffd9c7206430f14064c1ca75462
Ellias <113124292+IyaadArshad@users.noreply.github.com>
2025-02-12 14:35:21 +0400

    removed warnings

--- app/page.tsx
+++ app/page.tsx
@@ -1133,7 +1133,7 @@ const fetchAIResponse = async (userMessage: Message) => {
 
                   <div className="">
                     <div className="max-w-3xl mx-auto p-4">
-                        <div className="relative sticky bottom-0 bg-[#1E1E1E] p-4">
+                        <div className="sticky bottom-0 bg-[#1E1E1E] p-4">
                         <Input
                           value={message}
                           onChange={(e) => {
@@ -1357,7 +1357,7 @@ const fetchAIResponse = async (userMessage: Message) => {
 
                   <div className="">
                     <div className="max-w-3xl mx-auto p-4">
-                        <div className="relative sticky bottom-0 bg-[#1E1E1E] p-4">
+                        <div className="sticky bottom-0 bg-[#1E1E1E] p-4">
                         <Input
                           value={message}
                           onChange={(e) => {
@@ -1502,7 +1502,7 @@ const fetchAIResponse = async (userMessage: Message) => {
 
             <div className="">
               <div className="max-w-5xl mx-auto p-4">
-                  <div className="relative sticky bottom-0 bg-[#1E1E1E] p-4">
+                  <div className="sticky bottom-0 bg-[#1E1E1E] p-4">
                   <Input
                     value={message}
                     onChange={(e) => {
