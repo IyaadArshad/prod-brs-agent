ae55bb06317532c43a6638c2e130580dc81084c5
Ellias <113124292+IyaadArshad@users.noreply.github.com>
2025-03-09 08:31:45 +0400

    added google search api route

---
+++ app/api/v1/search/route.ts
@@ -0,0 +1,75 @@
+import axios from "axios";
+
+interface SearchResult {
+  title: string;
+  link: string;
+  snippet: string;
+}
+
+function cleanSearchResults(items: any[]): SearchResult[] {
+  return items.map((item) => ({
+    title: item.title || "",
+    link: item.link || "",
+    snippet: item.snippet || "",
+  }));
+}
+
+async function search(
+  searchItem: string,
+  apiKey: string,
+  cseId: string,
+  searchDepth: number = 10,
+  siteFilter?: string
+): Promise<SearchResult[]> {
+  const serviceUrl = "https://www.googleapis.com/customsearch/v1";
+
+  const params = {
+    q: searchItem,
+    key: apiKey,
+    cx: cseId,
+    num: searchDepth.toString(),
+  };
+
+  try {
+    const response = await axios.get(serviceUrl, { params });
+    const results = response.data;
+
+    if (results.items) {
+      const cleanedResults = cleanSearchResults(results.items);
+      if (siteFilter) {
+        return cleanedResults.filter((result) =>
+          result.link.includes(siteFilter)
+        );
+      }
+      return cleanedResults;
+    } else {
+      console.log("No search results found.");
+      return [];
+    }
+  } catch (error) {
+    console.error(`An error occurred during the search: ${error}`);
+    return [];
+  }
+}
+
+export function GET(request: Request) {
+  // Extract query parameters from request params
+  const url = new URL(request.url);
+  const searchItem = url.searchParams.get("query");
+  const key = process.env.GOOGLE_SEARCH_API_KEY;
+  const cx = process.env.GOOGLE_CSE_ID; // Assuming you have stored your CSE ID in an environment variable
+
+  if (!searchItem || !key || !cx) {
+    return new Response(
+      "Missing required query parameters or environment variables",
+      { status: 400 }
+    );
+  }
+
+  return search(searchItem, key, cx)
+    .then((results) => new Response(JSON.stringify(results), { status: 200 }))
+    .catch(
+      (error) =>
+        new Response(`An error occurred: ${error.message}`, { status: 500 })
+    );
+}
