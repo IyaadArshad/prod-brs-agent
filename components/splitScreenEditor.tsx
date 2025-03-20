54984b9e8ee17031f10f377a58b131added6fe60
Ellias <113124292+IyaadArshad@users.noreply.github.com>
2025-03-20 16:04:49 +0400

    change split screen background

--- components/splitScreenEditor.tsx
+++ components/splitScreenEditor.tsx
@@ -43,22 +43,25 @@ export function SplitScreenEditor({ markdown }: SplitScreenEditorProps) {
     <>
       <style>{`
       .milkdown-editor, .milkdown-editor * {
-        color: #fff !important;
-        font-family: 'Roboto', sans-serif !important;
+      color: #fff !important;
+      font-family: 'Roboto', sans-serif !important;
       }
       .milkdown-editor p {
-        margin-bottom: 1.1em !important;
-        font-size: 1.2rem !important;
-        line-height: 1.4 !important;
+      margin-bottom: 1.1em !important;
+      font-size: 1.2rem !important;
+      line-height: 1.4 !important;
       }
       .milkdown-editor milkdown-code-block,
       .milkdown-editor milkdown-code-block * {
-        color: #333 !important;
-        background-color: #f5f5f5 !important;
-        font-family: 'Roboto Mono', monospace !important;
+      color: #333 !important;
+      background-color: #f5f5f5 !important;
+      font-family: 'Roboto Mono', monospace !important;
       }
       `}</style>
-      <div ref={editorRef} className="milkdown-editor h-full w-full" />
+      <div
+        ref={editorRef}
+        className="milkdown-editor bg-[#2f2f2f] h-full w-full"
+      />
     </>
   );
 }
